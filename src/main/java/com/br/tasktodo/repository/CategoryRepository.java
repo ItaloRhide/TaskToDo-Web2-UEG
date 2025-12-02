4ecfe5a828ca8b1a9a3c977eb23888295a826e2d
Ítalo <109933525+ItaloRhide@users.noreply.github.com>
2025-12-02 20:03:19 -0300

    Delete src/main directory

--- src/main/java/com/br/tasktodo/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.tasktodo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.br.tasktodo.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
